fc9edbaf08c158ab6f84dbd23fcd1f4d0f6d1c0b
RafieqAhmed220 <rafieqahmed86@gmail.com>
2024-06-23 15:44:54 +0500

    Add files via upload
    assingment £02

---
+++ BANO QABIL/first.py
@@ -0,0 +1,23 @@
+a=7
+b=8
+form=0.5
+print("area_of_triangle",form*a*b)
+
+a=5
+b=7
+form=0.5
+print(a*b)
+
+a=6
+b=9
+form=3.14
+print(form*a*b)
+
+
+
+# name=int input("number")
+# input(name)
+name=int(input("your number"))
+print(type(name))
+
+
---
+++ BANO QABIL/first1.py
@@ -0,0 +1,6 @@
+name="Rafiq"
+print(name)
+last_name="Ahmed"
+print(last_name)
+last_name="shaikh"
+print(last_name)
